2c393cb024ef94401421f452848bf10dbc76fe8b
einsqing <786699892@qq.com>
2014-06-11 15:18:09 +0800

    Update s4.php

--- Install/templates/s4.php
+++ Install/templates/s4.php
@@ -10,7 +10,7 @@
             <?php require './templates/header.php'; ?>
             <section class="section">
                 <div class="">
-                    <div class="success_tip cc"> <a href="/Admin" class="f16 b">安装完成，进入后台管理</a>
+                    <div class="success_tip cc"> <a href="../index.php" class="f16 b">安装完成，进入后台管理</a>
                    </div>
                     <div class=""> </div>
                 </div>
