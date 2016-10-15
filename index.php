8435f7cf6b8f7aab97fb6e049e1a7c798e473afe
heqing <786699892@qq.com>
2016-10-15 15:12:35 +0800

    update v3.5.2

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
-define('APP_VERSION', '3.5.0');
+define('APP_VERSION', '3.5.2');
 define('APP_VERSION_TIME', '2016.10.15');
 // 定义应用目录
 define('APP_PATH','./Application/');
